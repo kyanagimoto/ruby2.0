70c763584a5053736c52c6dab32d6e671b3bcc3c
byplayer <byplayer100@gmail.com>
2013-01-22 18:19:14 +0900

    refactor spec

--- spec/bsearch_spec.rb
+++ spec/bsearch_spec.rb
@@ -1,6 +1,5 @@
 describe "bsearch" do
-  before(:all) { @array = (1..300).to_a }
   subject { (1..300).to_a }
-  it { @array.bsearch{ |i| 50 <=> i }.should == 50 }
-  it { @array.bsearch{ |i| i <=> 50 }.should be_nil }
+  it { subject.bsearch{ |i| 50 <=> i }.should == 50 }
+  it { subject.bsearch{ |i| i <=> 50 }.should be_nil }
 end
