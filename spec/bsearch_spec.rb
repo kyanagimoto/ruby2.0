70bc348e356ffbd21276dd060ae8752c82a4c002
byplayer <byplayer100@gmail.com>
2013-01-25 16:38:55 +0900

    g add greater equal pattern

--- spec/bsearch_spec.rb
+++ spec/bsearch_spec.rb
@@ -3,3 +3,11 @@ describe "bsearch" do
   it { subject.bsearch{ |i| 50 <=> i }.should == 50 }
   it { subject.bsearch{ |i| i <=> 50 }.should be_nil }
 end
+
+describe "bsearch greater equal" do
+  subject { [0, 3, 5, 10, 12] }
+  it { subject.bsearch{ |i| i >= 3 }.should == 3 }
+  it { subject.bsearch{ |i| i >= 4 }.should == 5 }
+  it { subject.bsearch{ |i| i >= -1 }.should == 0 }
+  it { subject.bsearch{ |i| i >= 20 }.should be_nil }
+end
